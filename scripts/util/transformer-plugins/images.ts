71f3921c252e0ba30e3d950a71867ea7049559fa
Jay Wick <dev@jaywick.xyz>
2020-09-06 19:24:57 +1000

    Generate image srcSets for responsive images

--- scripts/util/transformer-plugins/images.ts
+++ scripts/util/transformer-plugins/images.ts
@@ -6,21 +6,33 @@ import { Node } from 'unist'
 
 type ImageNode = Node & {
     tagName: string
-    properties: { [key: string]: unknown }
+    properties: { [key: string]: string | null }
 }
 
 export const lazyImages = () => (tree: Node) => {
-    function visitor(node: ImageNode) {
-        const {
-            tagName,
-            properties: { src, srcSet },
-        } = node
-
-        if (tagName !== 'img' || typeof src !== 'string' || srcSet) {
+    function visitor({ tagName, properties }: ImageNode) {
+        if (tagName !== 'img') {
             return
         }
 
-        node.properties.loading = 'lazy'
+        if (!properties.loading) {
+            properties.loading = 'lazy'
+        }
+
+        if (!properties.srcSet && properties.src) {
+            const isExternal =
+                properties.src.startsWith('http') ||
+                properties.src.startsWith('//')
+
+            if (!isExternal) {
+                const { name, ext } = paths.parse(properties.src)
+                properties.srcSet = ['320w', '480w', '768w', '1024w']
+                    .map((size) => `${name}-${size}${ext} ${size}`)
+                    .concat(`${name}${ext} 1200w`)
+                    .join(', ')
+                properties.sizes = '100vw'
+            }
+        }
     }
 
     visit(tree, ['element'], visitor)
